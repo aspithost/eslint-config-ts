796579634b7ef953c1c961d4ef5af4f67913db79
aspithost <abelspithost@gmail.com>
2026-04-15 22:06:35 +0200

    chore: bump version

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'eslint/config';
-import { createConfig } from './src/index.js';
+import { createConfig } from '@/index.js';
 
 const eslintConfig: Config[] = createConfig();
 export default eslintConfig;
